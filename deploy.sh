cd2bba76643129d881930e6bf847495e74aa3d6f
Brian Whicheloe <brian@Brians-MacBook-Pro-3.local>
2018-09-25 13:22:01 -0700

    Update deploy script and add update checker

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+# This script is automatically run by updater.js whenever it detects an update to the software repository
+# It can also be run manually
+
 git pull
+npm install
 npm run pack-production
-npm run electron
+pm2 startOrRestart app.json
---
+++ updater.js
@@ -0,0 +1,41 @@
+const fs = require('fs')
+const axios = require('axios')
+const exec = require('child_process').exec
+
+// Make a blank text file if one doesn't exist yet to prevent errors on subsequent attempted reads
+if (!fs.existsSync('./last-update.txt')) {
+    fs.writeFileSync('./last-update.txt', '')
+}
+
+// Get the time of the last software update from a text file
+const lastUpdate = fs.readFileSync('./last-update.txt', 'utf8')
+
+// Poll the software respository for the time of the latest update
+async function checkForUpdates() {
+    let response = await axios.get('https://api.github.com/repos/briancw/tvmenu')
+
+    let {data: {updated_at}} = response
+
+    if (updated_at) {
+        if (updated_at !== lastUpdate) {
+            // If time of the latest change doesn't match the last known time, save it
+            fs.writeFileSync('./last-update.txt', updated_at)
+            console.log(`Software updated\nLast update: ${updated_at}`)
+
+            // Run deploy.sh, which pulls the latest code, re-install dependencies, re-builds the application, and restarts it
+            exec('./deploy.sh', (error, stdout, stderr) => {
+                if (error) {
+                    console.error(error)
+                }
+
+                console.log(stdout)
+            })
+        }
+    } else {
+        console.error('Error getting last update time')
+    }
+}
+
+// Check for updates every 10 minutes
+const updateCheckTime = 1000 * 60 * 10
+setInterval(checkForUpdates, updateCheckTime)
