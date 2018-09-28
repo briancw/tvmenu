0917ff64c7125a4a84aa9f29eacc29021de82526
Brian Whicheloe <brian@whicheloe.us>
2018-09-28 12:29:08 -0700

    Do a git reset before pull on deploy

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@
 # This script is automatically run by updater.js whenever it detects an update to the software repository
 # It can also be run manually
 
+git reset --hard
 git pull
 npm install
 npm run pack-production
