aa03e1e65775b6c78bcc0f8f7871a52a751bdc67
Brian Whicheloe <brian@Brians-MacBook-Pro-3.local>
2018-10-06 10:32:10 -0700

    Update updater and remove burgers

--- updater.js
+++ updater.js
@@ -1,18 +1,28 @@
 const fs = require('fs')
 const axios = require('axios')
+const http2 = require('http2')
 const exec = require('child_process').exec
+const softwareVersionFile = 'last-update-software.txt'
+const contentVersionFile = 'last-update-content.txt'
+const contentFile = 'content.json'
+const {userId} = require('./config.js')
+const apiServer = 'https://local.whicheloe.us:3001'
+// const apiServer = 'https://signage.whicheloe.us'
 
 // Make a blank text file if one doesn't exist yet to prevent errors on subsequent attempted reads
-if (!fs.existsSync('./last-update.txt')) {
-    fs.writeFileSync('./last-update.txt', '')
-    console.log('Creating new last-update.txt\n\n')
+if (!fs.existsSync(softwareVersionFile)) {
+    fs.writeFileSync(softwareVersionFile, '')
+}
+if (!fs.existsSync(contentVersionFile)) {
+    fs.writeFileSync(contentVersionFile, '')
 }
 
 // Get the time of the last software update from a text file
-const lastUpdate = fs.readFileSync('./last-update.txt', 'utf8')
+const lastUpdateSoftware = fs.readFileSync(softwareVersionFile, 'utf8')
+const lastUpdateContent = fs.readFileSync(contentVersionFile, 'utf8')
 
 // Poll the software respository for the time of the latest update
-async function checkForUpdates() {
+async function checkForSoftwareUpdate() {
     let response = await axios.get('https://api.github.com/repos/briancw/tvmenu')
 
     let {data: {updated_at}} = response
@@ -22,9 +32,9 @@ async function checkForUpdates() {
     console.log(`Last known version: ${lastUpdate}\n`)
 
     if (updated_at) {
-        if (updated_at !== lastUpdate) {
+        if (updated_at !== lastUpdateSoftware) {
             // If time of the latest change doesn't match the last known time, save it
-            fs.writeFileSync('./last-update.txt', updated_at)
+            fs.writeFileSync(softwareVersionFile, updated_at)
             console.log(`Software updated\nLast update: ${updated_at}`)
 
             // Run deploy.sh, which pulls the latest code, re-install dependencies, re-builds the application, and restarts it
@@ -41,6 +51,46 @@ async function checkForUpdates() {
     }
 }
 
+async function checkForContentUpdate() {
+    // let response = await axios.get(`${apiServer}/content`, {
+    //     headers: {
+    //         userId,
+    //     }
+    // })
+    let session = http2.connect(`${apiServer}`)
+    let request = session.request({
+        ':path': '/content',
+        userId,
+    })
+
+    request.on('response', (headers) => {
+        let data = ''
+
+        request.on('data', (chunk) => {
+ data += chunk })
+        .on('end', () => {
+            console.log(data)
+        })
+    })
+
+    // let {updateTime, content} = response
+
+    // if (updateTime) {
+    //     if (updateTime !== lastUpdateContent) {
+    //         // If time of the latest change doesn't match the last known time, save it
+    //         fs.writeFileSync(contentVersionFile, updateTime)
+    //         console.log(`Content updated\nLast update: ${updateTime}`)
+
+    //         fs.writeFileSync(contentFile, content)
+    //     }
+    // }
+}
+
 // Check for updates every 10 minutes
-const updateCheckTime = 1000 * 60 * 5
-setInterval(checkForUpdates, updateCheckTime)
+const softwareInterval = 1000 * 60 * 5
+const contentInterval = 1000 * 60
+
+setInterval(checkForSoftwareUpdate, softwareInterval)
+// setInterval(checkForContentUpdate, contentInterval)
+
+// checkForContentUpdate()
