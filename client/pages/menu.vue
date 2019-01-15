4967c2d1bbe26a3bb1e19a8f6d606b001e6d940a
Brian Whicheloe <brian@whicheloe.us>
2019-01-14 22:12:15 -0800

    Big design changes and re-add specialty burgers

--- client/pages/menu.vue
+++ client/pages/menu.vue
@@ -1,13 +1,16 @@
 <template>
     <div class="menuWrapper">
         <div class="title">{{ pageContent.displayTitle }}</div>
+        <div class="pageDescription">{{ pageContent.description }}</div>
         <div class="items">
-            <div v-for="(item, itemIndex) in pageContent.items" class="menuItem" :key="itemIndex">
-                <!-- <img class="menuPic" v-if="item.img" src="http://placehold.it/100x100" /> -->
-                <div class="itemDetails">
-                    <div class="menuItemTitle" v-html="nl2br(item.title)"></div>
-                    <div class="menuItemDescription" v-if="item.description" v-html="nl2br(item.description)"></div>
-                    <div class="menuItemPrice" v-if="item.price">${{ item.price.toFixed(2) }}</div>
+            <div v-for="(column, columnIndex) in pageContent.columns" class="menuColumn" :key="columnIndex">
+                <div v-for="(item, itemIndex) in column" class="menuItem" :key="itemIndex">
+                    <!-- <img class="menuPic" v-if="item.img" src="http://placehold.it/100x100" /> -->
+                    <div class="itemDetails">
+                        <div class="menuItemTitle" v-html="nl2br(item.title)"></div>
+                        <div class="menuItemDescription" v-if="item.description" v-html="nl2br(item.description)"></div>
+                        <div class="menuItemPrice" v-if="item.price">${{ item.price.toFixed(2) }}</div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -45,67 +48,85 @@ export default {
             let res = await fetch('/menu.json')
             let menuContent = await res.json()
 
-            menuContent.pages.forEach(page => {
+            menuContent.pages.forEach((page) => {
                 if (page.pageName === this.menuPage) {
                     this.pageContent = page
                 }
             })
-        }
-    }
+        },
+    },
 }
 </script>
 
 <style lang="less">
 @import '../styles/mixins.less';
 
+body {
+    max-height: 1050px;
+}
+
 .menuWrapper {
     // display: flex;
     width: 100%;
     margin: 0px auto;
     padding-top: 20px;
-    // font-family: 'Ubuntu';
     font-family: 'Baloo Tammudu';
     text-shadow: 4px 4px 0px #000;
     cursor: none;
+    letter-spacing: 3px;
+    word-spacing: 5px;
 
     .title {
         font-size: 100px;
         text-align: center;
-        padding-top: 10px;
+        padding-top: 30px;
         color: yellow;
     }
 
+    .pageDescription {
+        width: 90%;
+        margin: 0px auto;
+        text-align: center;
+        font-size: 46px;
+        padding: 10px 0px 0px;
+    }
+
     .items {
-        padding-top: 10px;
+        padding-top: 85px;
         width: 90%;
         margin: 0px auto;
 
-        .menuItem {
-            clear: both;
-            margin-bottom: 20px;
+        .menuColumn {
+            width: 50%;
+            float: left;
 
-            .menuPic {
-                display: inline-block;
-            }
+            .menuItem {
+                clear: both;
+                margin-bottom: 40px;
 
-            .itemDetails {
-                display: inline-block;
-                vertical-align: top;
-                padding-top: 5px;
-
-                .menuItemTitle {
-                    font-size: 40px;
-                    color: yellow;
+                .menuPic {
+                    display: inline-block;
                 }
 
-                .menuItemDescription {
-                    font-size: 30px;
-                    padding: 10px 10px;
-                }
+                .itemDetails {
+                    display: inline-block;
+                    vertical-align: top;
+                    padding-top: 5px;
 
-                .menuItemPrice {
-                    font-size: 32px;
-                    padding: 5px 10px 0px 10px;
+                    .menuItemTitle {
+                        font-size: 60px;
+                        color: yellow;
+                    }
+
+                    .menuItemDescription {
+                        font-size: 40px;
+                        padding: 10px 10px;
+                    }
+
+                    .menuItemPrice {
+                        font-size: 46px;
+                        padding: 5px 10px 0px 10px;
+                    }
                 }
             }
         }
